149e2e6e74ea37e5a2a8963f2595927604af260b
Yuichiro Toriyama <yuichiro.toriyama@cach-inc.com>
2019-03-29 04:22:04 +0000

    first commit

---
+++ sigfox/client.go
@@ -0,0 +1,105 @@
+package sigfox
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"reflect"
+
+	"github.com/google/go-querystring/query"
+	"github.com/pkg/errors"
+)
+
+const (
+	defaultBaseURL = "https://api.sigfox.com/v2"
+)
+
+type Client struct {
+	baseURL    *url.URL
+	HTTPClient *http.Client
+
+	Login, Password string
+	UserAgent       string
+
+	common service
+
+	Devices *DevicesService
+}
+
+type service struct {
+	client *Client
+}
+
+type Pagination struct {
+	Next string `json:"next"`
+}
+
+func NewClient(login, password string) (*Client, error) {
+	if len(login) == 0 {
+		return nil, errors.New("missing login key")
+	}
+
+	if len(password) == 0 {
+		return nil, errors.New("missing login password")
+	}
+
+	parsedURL, err := url.ParseRequestURI(defaultBaseURL)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to parse url: %s", defaultBaseURL)
+	}
+
+	c := &Client{HTTPClient: &http.Client{}, baseURL: parsedURL, Login: login, Password: password}
+	c.common.client = c
+	c.Devices = (*DevicesService)(&c.common)
+
+	return c, nil
+}
+
+func (c *Client) newRequest(ctx context.Context, method, spath string, body io.Reader) (*http.Request, error) {
+	u := *c.baseURL
+	u.Path = path.Join(c.baseURL.Path, spath)
+
+	req, err := http.NewRequest(method, u.String(), body)
+	if err != nil {
+		return nil, err
+	}
+
+	req = req.WithContext(ctx)
+	req.SetBasicAuth(c.Login, c.Password)
+	req.Header.Set("Content-Type", "application/json")
+
+	return req, nil
+}
+
+func decodeBody(resp *http.Response, out interface{}) error {
+	defer resp.Body.Close()
+
+	if w, ok := out.(io.Writer); ok {
+		io.Copy(w, resp.Body)
+	}
+	decoder := json.NewDecoder(resp.Body)
+	return decoder.Decode(out)
+}
+
+func addOptions(s string, opt interface{}) (string, error) {
+	v := reflect.ValueOf(opt)
+	if v.Kind() == reflect.Ptr && v.IsNil() {
+		return s, nil
+	}
+
+	u, err := url.Parse(s)
+	if err != nil {
+		return s, err
+	}
+
+	vs, err := query.Values(opt)
+	if err != nil {
+		return s, err
+	}
+
+	u.RawQuery = vs.Encode()
+	return u.String(), nil
+}
---
+++ sigfox/devices.go
@@ -0,0 +1,127 @@
+package sigfox
+
+import (
+	"context"
+	"fmt"
+)
+
+type DevicesService service
+
+type Device struct {
+	ID                  string  `json:"id"`
+	Name                string  `json:"name"`
+	Prototype           bool    `json:"prototype"`
+	PAC                 string  `json:"pac"`
+	SequenceNumber      int32   `json:"sequenceNumber"`
+	TrashSequenceNumber int32   `json:"trashSequenceNumber"`
+	LastCom             int     `json:"lastCom"`
+	AverageSnr          float32 `json:"averageSnr"`
+	AverageRssi         float32 `json:"averageRssi"`
+	ActivationTime      int64   `json:"activationTime"`
+	CreationTime        int64   `json:"creationTime"`
+	State               int32   `json:"state"`
+	ComState            int32   `json:"comState"`
+}
+
+type DeviceListOptions struct {
+	ID           string   `url:"id"`
+	GroupIds     []string `url:"groupIds"`
+	Deep         bool     `url:"deep"`
+	DeviceTypeId string   `url:"deviceTypeId"`
+	OperatorId   string   `url:"operatorId"`
+	Sort         string   `url:"sort"`
+	MinId        string   `url:"minId"`
+	MaxId        string   `url:"maxId"`
+	Fields       []string `url:"fields"`
+	Limit        int32    `url:"limit"`
+	Offset       int32    `url:"offset"`
+	PageId       string   `url:"pageId"`
+}
+
+type ListDevices struct {
+	Data   []Device   `json:"data"`
+	Paging Pagination `json:"paging"`
+}
+
+func (s *DevicesService) List(ctx context.Context, opt *DeviceListOptions) (*ListDevices, error) {
+	spath := fmt.Sprintf("/devices")
+	spath, err := addOptions(spath, opt)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := s.client.newRequest(ctx, "GET", spath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := s.client.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var listDevices ListDevices
+	if err := decodeBody(res, &listDevices); err != nil {
+		return nil, err
+	}
+
+	return &listDevices, nil
+}
+
+type DeviceMessages struct {
+	Data []Message `json:"data"`
+}
+
+type DeviceMessagesOptions struct {
+	Limit int `url:"limit"`
+}
+
+type Message struct {
+	Time int    `json:"time"`
+	Data string `json:"data"`
+}
+
+func (s *DevicesService) GetInfo(ctx context.Context, deviceID string) (*Device, error) {
+	spath := fmt.Sprintf("/devices/%s", deviceID)
+	req, err := s.client.newRequest(ctx, "GET", spath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := s.client.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var device Device
+	if err := decodeBody(res, &device); err != nil {
+		return nil, err
+	}
+
+	return &device, nil
+}
+
+func (s *DevicesService) GetMessages(ctx context.Context, deviceID string, opt *DeviceMessagesOptions) (*DeviceMessages, error) {
+	spath := fmt.Sprintf("/devices/%s/messages", deviceID)
+	spath, err := addOptions(spath, opt)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := s.client.newRequest(ctx, "GET", spath, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := s.client.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var messages DeviceMessages
+	if err := decodeBody(res, &messages); err != nil {
+		return nil, err
+	}
+
+	return &messages, nil
+}
